2d68c3b94ad471f3c188d75b8c6fa53dfb0f4ea5
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2023-08-06 12:36:11 -0700

    change future projects

--- src/components/hs/projectDescs/FutureProjectsDesc.tsx
+++ src/components/hs/projectDescs/FutureProjectsDesc.tsx
@@ -13,22 +13,20 @@ const FutureProjectsDesc = () => {
                     {`Below is a list of some things I'd be interested in making!
                     Keep in mind that these are not guaranteed to happen!`}
                     <ul className="list-disc px-10 pt-2">
-                        <li>
+                        {/* <li>
                             <b>Pestercord Descend</b>:
-                            {` I'm actually pretty close to being done this one,
+                            {`I'm actually pretty close to being done this one,
                             but essentially, this is a way to use Pestercord
                             without needing BetterDiscord. This would allow for
                             Pestercord to be used on MacOS and Linux, too,
                             which is not possible with regular Pestercord!`}
-                        </li>
+                        </li> */}
                         <li>
-                            <b>Emblem of Picking</b>:
-                            {` This would be a Fire Emblem Engage project!
-                            The goal would be to act as a tool for managing
-                            a Fire Emblem Engage PMU. You'd feed it your list
-                            for the PMU, and then when you select a chapter or
-                            map, it would tell you which units you'll be
-                            deploying for that map.`}
+                            <b>Word of Jayden</b>:
+                            {`This is a project built to be a resource for BrownDust2.
+                            Right now, I'm just planning on making it have a list of
+                            characters and their costumes and skills, but I'd like it
+                            to be more fleshed out in the future!`}
                         </li>
                     </ul>
                 </p>
